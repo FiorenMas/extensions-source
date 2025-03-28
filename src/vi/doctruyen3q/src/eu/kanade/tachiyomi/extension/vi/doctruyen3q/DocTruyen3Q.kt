d4fb66814bd54afdc4f0624be0ee2f9165fbd921
Fioren <102145692+FiorenMas@users.noreply.github.com>
2025-03-29 02:24:37 +0700

    Fix image not load DocTruyen3Q
    Fix image not load, update domain DocTruyen3Q

--- src/vi/doctruyen3q/src/eu/kanade/tachiyomi/extension/vi/doctruyen3q/DocTruyen3Q.kt
+++ src/vi/doctruyen3q/src/eu/kanade/tachiyomi/extension/vi/doctruyen3q/DocTruyen3Q.kt
@@ -23,7 +23,7 @@ import java.util.TimeZone
 class DocTruyen3Q :
     WPComics(
         "DocTruyen3Q",
-        "https://doctruyen3qui.pro",
+        "https://doctruyen3qui5.pro",
         "vi",
         dateFormat = SimpleDateFormat("dd-MM-yyyy", Locale.ROOT).apply {
             timeZone = TimeZone.getTimeZone("Asia/Ho_Chi_Minh")
@@ -37,8 +37,14 @@ class DocTruyen3Q :
         .build()
 
     override fun pageListParse(document: Document): List<Page> {
-        return document.select(".page-chapter[id] a img, .page-chapter[id] img").mapIndexed { index, element ->
-            val img = element.attr("abs:src").takeIf { it.isNotBlank() } ?: element.attr("abs:data-original")
+        return document.select(".page-chapter[id] img").mapIndexed { index, element ->
+            val img = element.attr("abs:src").let { url ->
+                if (url.startsWith("//")) {
+                    "https:$url"
+                } else {
+                    url
+                }
+            }
             Page(index, imageUrl = img)
         }.distinctBy { it.imageUrl }
     }
